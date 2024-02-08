bb90d27a11a36077db3b980fb14ec9dd37612ca0
CelalMavi <celal.mavi.cm@gmail.com>
2024-02-08 12:54:27 +0300

    MaskeTakip

---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+class Program()
+{
+
+    static void Main(String[] args)
+    {
+        Person person1 = new Person();
+        person1.FirstName = "Celal";
+        person1.LastName = "Mavi";
+        person1.DateOfBirthYear = 1999;
+        person1.NationalId = 123;
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+
+    }
+
+
+}
